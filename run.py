9d5d9e534c5731c5e4704a553ac6098b8d256534
Jonathan Griffiths <grifmo@gmail.com>
2013-10-05 19:17:42 +0200

    changed run.py to hopefully get it working on heroku

--- run.py
+++ run.py
@@ -1,6 +1,15 @@
+import os
 from eve import Eve
-app = Eve()
-app.HATEOAS = False
 
 if __name__ == '__main__':
-    app.run()
+    # Heroku support: bind to PORT if defined, otherwise default to 5000.
+    if 'PORT' in os.environ:
+        port = int(os.environ.get('PORT'))
+        host = '0.0.0.0'
+    else:
+        port = 5000
+        host = '127.0.0.1'
+
+    app = Eve()
+	app.HATEOAS = False
+    app.run(host=host, port=port)
