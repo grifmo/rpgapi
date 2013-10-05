ab119e7f77e6148a465d8f79a552c39c61bf32a1
Jonathan Griffiths <grifmo@gmail.com>
2013-10-05 19:21:58 +0200

    Heroku update on run.py

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import os
 from eve import Eve
-
+app = Eve()
+app.HATEOAS = False
 if __name__ == '__main__':
     # Heroku support: bind to PORT if defined, otherwise default to 5000.
     if 'PORT' in os.environ:
@@ -10,6 +11,4 @@ if __name__ == '__main__':
         port = 5000
         host = '127.0.0.1'
 
-    app = Eve()
-	app.HATEOAS = False
-    app.run(host=host, port=port)
+	app.run(host=host, port=port)
