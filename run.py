a46e763125df726ff3f630fef9fd2eca2ef86284
Jonathan Griffiths <grifmo@gmail.com>
2013-10-05 21:21:28 +0200

    Yet another run.py change

--- run.py
+++ run.py
@@ -1,16 +1,29 @@
 # -*- coding: utf-8 -*-
 
+"""
+    RPG API Demo
+    ~~~~~~~~
+
+    A demostration of a simple API powered by Eve REST API.
+
+    The live demo is available at eve-demo.herokuapp.com. Please keep in mind
+    that the it is running on Heroku's free tier using a free MongoHQ
+    sandbox, which means that the first request to the service will probably
+    be slow. The database gets a reset every now and then.
+
+"""
+
 import os
 from eve import Eve
-app = Eve()
-app.HATEOAS = False
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     # Heroku support: bind to PORT if defined, otherwise default to 5000.
-    if "PORT" in os.environ:
-        port = int(os.environ.get("PORT", 5000))
+    if 'PORT' in os.environ:
+        port = int(os.environ.get('PORT'))
         host = '0.0.0.0'
     else:
         port = 5000
         host = '127.0.0.1'
 
-	app.run(host=host, port=port)
+    app = Eve()
+    app.run(host=host, port=port)
